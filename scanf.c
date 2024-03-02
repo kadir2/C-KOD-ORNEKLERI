12af70117f28132db7601682c947d6637df8c08e
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-03-03 00:24:32 +0300

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+float sayi1;
+float sayi2;
+
+printf("1. sayı:");
+scanf("%f",&sayi1);
+
+printf("2. sayı");
+scanf("%f",&sayi2);
+
+printf("%.3f",sayi1+sayi2);
+
+
+return 0;
+
+}
