477697774bb48269af872bbb51e29f51930af78e
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-03-13 23:22:01 +0300

    Add files via upload

---
+++ enum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+enum günler {pzt=1,sal,çrş,per,cum,cmt,pzr} ;
+void gün();
+
+int main(){
+
+
+enum aylar {ock,şbt,mrt,nsn,may,haz,tem,ağu,eyl,ekm,kas,ara}
+
+buay=mrt;
+
+printf("bu ay %d. ay\n", buay+1);
+
+gün();
+}
+
+void gün(){
+
+enum günler bugün;
+bugün=per;
+
+printf("bugün %d. gün",bugün);
+
+}
