504efbf65bb9d753afa01d5f76adf9dacaea7498
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-03-26 14:28:08 +0300

    Update rastgele.sayi.c

--- rastgele.sayi.c
+++ rastgele.sayi.c
@@ -7,10 +7,13 @@ int main(){
 srand(time(0)); // sayaç başlatılıyor
 
 int a;
+int b;
+  
+a= 1 + rand() % 100 ; // random sayı 1 ile 100 arasına sıkışıyor
 
-a= 1 + rand() % 100; // random sayı 1 ile 100 arasına sıkışıyor
+printf("%d\n",a);
 
-printf("%d",a);
-
+b= rand() % 100-50 ; // -50 ile 49 arası sayı üretiyor
+printf("%d",b);  
 return 0;
 }
