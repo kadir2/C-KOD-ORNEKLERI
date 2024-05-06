821b2a3aace619bb75123375445c2aa45cafa74e
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-05-06 22:21:28 +0300

    Create sayı_baski_1.c

---
+++ Formatted input-output/sayı_baski_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(){
+
+printf("%d\n",125);
+printf("%i\n",125); //ingegers 
+printf("%d\n",+125);
+printf("%d\n",-125);
+printf("%hd\n",-1200);      //short
+printf("%ld\n",120000000000L);      //long float
+printf("%o\n",125);      //octal (8 lik) integer
+printf("%u\n",125);      //unsigned integer
+printf("%u\n",-125);
+printf("%x\n",125);         //hexadecimal (16 lık) integer
+printf("%X",125);       //hexadecimal (16 lık) integer
+
+
+return 0;
+}
