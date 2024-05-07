d9520b3b13efab3a9694096f3cc6d5435d6628d7
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-05-07 23:15:02 +0300

    Update dizi_oluştuturoruz_4.c

--- Characters and Strings/dizi_oluştuturoruz_4.c
+++ Characters and Strings/dizi_oluştuturoruz_4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+
+int main(void)
+{
+char c;
+
+char sentence [ 80 ];
+int i = 0; 
+
+puts("Enter a line of text:" );
+
+
+while (( c = getchar()) != '\n') {  //sıryla getchar c ye harfleri aktarır
+sentence[i++] = c;
+} 
+
+sentence[i] = '\0';
+puts("\nThe line entered was:");
+puts(sentence );
+return 0; 
+}
