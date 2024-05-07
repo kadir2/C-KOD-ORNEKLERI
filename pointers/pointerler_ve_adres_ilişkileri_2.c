c7860113939f5c252fd03bd189deb3d7a0e69a34
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-05-07 22:44:26 +0300

    Update pointerler_ve_adres_ilişkileri_2.c

--- pointers/pointerler_ve_adres_ilişkileri_2.c
+++ pointers/pointerler_ve_adres_ilişkileri_2.c
@@ -8,7 +8,7 @@ int *ptr;
 ptr=&a1;
 
 printf("a1 in adresi: %p \nptr nin değeri: %p \n\n",&a1,ptr); //p adres gösterimi için kullanılır
-printf("a1 in değeri: %d \n*ptr nin deperi: %d \n\n",a1,*ptr);
+printf("a1 in değeri: %d \n*ptr nin değeri: %d \n\n",a1,*ptr);
 printf("comlpement lerin gösterimi\n&*ptr : %p\n*&ptr : %p",&*ptr,*&ptr); //operatörler birbirini yok ediyor
 
 printf("\n\n%d\n%p",*&*ptr,&*&ptr); //başka bir yok etme gösterimi
